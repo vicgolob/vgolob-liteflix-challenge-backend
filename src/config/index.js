0a6252380bdca5bc57c3da1caf06d15150ed6d74
Victor Golob <victorgolob@gmail.com>
2023-09-29 01:10:42 -0300

    initial setup - config sequalize + postgres14 docker-compose

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	local: {
+		use_env_variable: "DATABASE_URL",
+	},
+	development: {
+		use_env_variable: "DATABASE_URL",
+	},
+	production: {
+		use_env_variable: "DATABASE_URL",
+	},
+};
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const { sequelize } = require("./models");
+const app = express();
+const port = process.env.PORT ?? 5000;
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(port, async () => {
+	console.log(`Server is listening on port ${port}`);
+	try {
+		await sequelize.authenticate();
+		await sequelize.sync({ force: false });
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+});
