b3cd231b59ddcc7bef6dbb0fc3852a01b0b041bf
Victor Golob <victorgolob@gmail.com>
2023-09-29 07:17:08 -0300

    update /movies routes

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getmovies } = require("../controllers/moviesController");
+const { getmovies, addMovie } = require("../controllers/moviesController");
 
 router.get("/", getmovies);
+router.post("/add", addMovie);
 
 module.exports = router;
