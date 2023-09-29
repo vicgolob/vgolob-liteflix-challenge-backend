fadf0222b58a9d9ca795eda39d3a34baa41c3a83
Victor Golob <victorgolob@gmail.com>
2023-09-29 03:29:45 -0300

    generate model with seeders and migrations

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 const express = require("express");
 
-const { sequelize } = require("./models");
+const { sequelize, Movie } = require("./models");
+
 const app = express();
 const port = process.env.PORT ?? 5000;
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
+});
+
+app.get("/movies", async (req, res) => {
+	let { page, pageSize } = req.query;
+	if (!page) {
+		page = 1;
+	}
+	if (!pageSize) {
+		pageSize = 5;
+	}
+	const offset = (page - 1) * pageSize;
+
+	try {
+		const movies = await Movie.findAll({
+			limit: pageSize,
+			offset,
+		});
+
+		const totalCount = await Movie.count();
+
+		const totalPages = Math.ceil(totalCount / pageSize);
+
+		const response = {
+			page: page,
+			results: movies,
+			total_pages: totalPages,
+			total_results: totalCount,
+		};
+
+		res.json(response);
+	} catch (error) {
+		console.error("DEBUG:: GET /movies - Error:", error);
+		res.status(500).json({ error: "Unable to get movies" });
+	}
 });
 
 app.listen(port, async () => {
